6c0cbdf03f84d0b24c3da9779baefdbade95e34b
alexsomeoddpilot <alex@someoddpilot.com>
2014-11-11 12:00:26 -0600

    Using yaml to output content

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function (factory) {
+  var jsyaml = require('js-yaml');
+
+  require('angular');
+
+  factory(window.angular, jsyaml);
+}(function factory (angular, jsyaml) {
+  angular.module('sop-doc', [])
+    .directive('content', function ($http) {
+      return {
+        restrict: 'E',
+        link: function (scope, element) {
+          $http.get('config.yaml')
+            .success(function (configYaml) {
+              var parts = [];
+
+              jsyaml.safeLoadAll(configYaml, function (config) {
+                parts.push(config);
+              });
+              element.html(parts[1]);
+            });
+        }
+      };
+    });
+}));
+
