61be89131a65e55f536e415f81c8acba429f7575
Alex Robertson <alex@someoddpilot.com>
2015-04-23 09:40:36 -0500

    Gathering chapters

--- tasks/html.js
+++ tasks/html.js
@@ -8,19 +8,19 @@ var fs = require('fs');
 var handlebars = require('handlebars');
 var _ = require('lodash');
 
+const headingRegex = /<(h[1-6]).*(id=".*")*>(.*)<\/\1>/g;
+
 function insertId(text, headingMatch, headingType) {
-  return '<' + headingType + ' id="' + _.kebabCase(text) + '">';
+  return '<' + headingType + ' id="' + _.kebabCase(_.unescape(text)) + '">';
 }
 
-function insertIdIntoHeadings(match, _headingType, text) {
+function insertIdIntoHeadings(match, _headingType, id, text) {
   return match
     .replace(/<(h[1-6])>/, insertId.bind(this, text));
 }
 
 function markdownTransform(file, enc, callback) {
   var parsed = reader.parse(file.contents.toString());
-
-  const headingRegex = /<(h[1-6])>(.*)<\/\1>/g;
 
   var html = writer.render(parsed)
     .replace(headingRegex, insertIdIntoHeadings);
@@ -31,9 +31,19 @@ function markdownTransform(file, enc, callback) {
 }
 
 function handlebarsTransform(file, enc, callback) {
+  var chapters = file.contents.toString()
+    .match(headingRegex)
+    .map(function (match) {
+      return {
+        anchor: match.match(/id="(.*)"/)[1],
+        title: match.match(/>(.*)</)[1]
+      };
+    });
+
   var html = handlebars.compile(fs.readFileSync('index.html', 'utf8'))({
     styles: fs.readFileSync('css/style.css', 'utf8'),
-    contents: file.contents.toString()
+    contents: file.contents.toString(),
+    chapters: chapters
   });
 
   file.contents = new Buffer(html);
