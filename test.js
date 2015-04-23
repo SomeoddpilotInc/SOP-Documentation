44c8f9113f0b21054e9fc4d186a33e5bde62168c
Alex Robertson <alex@someoddpilot.com>
2015-04-23 10:59:24 -0500

    Adjusting tests

---
+++ test.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var del = require('del');
+var fs = require('fs');
+var assert = require('assert');
+require('./gulpfile');
+
+describe('Documentation framework', function () {
+  beforeEach(function (done) {
+    del('build', done);
+  });
+
+  it('should create index.html file', function (done) {
+    gulp.start('html', function () {
+      assert.equal(fs.existsSync('build/index.html'), true);
+      done();
+    });
+  });
+});
